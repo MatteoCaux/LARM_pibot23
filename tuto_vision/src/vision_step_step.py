b14a6be7c3774bb0ac0fcaa8fb05a1ed18c03e9c
MAILLET - 07 <kristianb851@gmail.com>
2025-01-14 11:53:26 +0100

    up camera with align depth

--- tuto_vision/src/vision_step_step.py
+++ tuto_vision/src/vision_step_step.py
@@ -5,6 +5,20 @@ import matplotlib.pyplot as plt
 # from skimage.measure import label, regionprops
 import math
 
+# connect to a sensor (0: webcam)
+pipeline = rs.pipeline()
+config = rs.config()
+
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+
+config.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 60)
+config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 60)
+
+# Start streaming
+pipeline.start(config)
+
+
 def filtre_vert(image_couleur):
     hsv = cv2.cvtColor(image_couleur, cv2.COLOR_BGR2HSV)
     
@@ -19,45 +33,46 @@ def filtre_vert(image_couleur):
 
     closing=cv2.morphologyEx(mask_vert,cv2.MORPH_OPEN,kernel)
 
-    res2_vert = cv2.cvtColor(cv2.bitwise_and(image,image,mask=closing),cv2.COLOR_HSV2BGR)
+    res2_vert = cv2.cvtColor(cv2.bitwise_and(hsv,hsv,mask=closing),cv2.COLOR_HSV2BGR)
 
     return closing,res2_vert
 
-def contours(image_couleur,mask,vert):
+def contours(image_couleur,mask,vert,depth_frame):
     contours=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     if len(contours) > 0:
         for c in contours :
             ((x, y), rayon)=cv2.minEnclosingCircle(c)
             rect=cv2.boundingRect(c)
-            print(rect)
-            print(rayon)
+            #print(rect)
+            #print(rayon)
             coin=rect[:2]
             largeur=rect[2]
             hauteur=rect[3]
             if rayon>20:
                 cv2.circle(vert, (int(x), int(y)), int(rayon), color_info, 2)
                 cv2.rectangle(image_couleur, coin, (coin[0]+largeur, coin[1]+hauteur),color_info, 2)
+                coord = depth_of_selection(int(x),int(y),depth_frame)
+                print(coord)
 
-def template_matching(template_path, image):
-    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# def template_matching(template_path, image):
+#     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # charger le template de l'objet à rechercher
-    template = cv2.imread(template_path,0)
+#     # charger le template de l'objet à rechercher
+#     template = cv2.imread(template_path,0)
 
-    # Récupération des dimensions de l'image
-    w, h = template.shape[::-1]
+#     # Récupération des dimensions de l'image
+#     w, h = template.shape[::-1]
 
-    # Application du template atching
-    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+#     # Application du template atching
+#     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
 
-    # Sélection des meilleurs matched objects
-    threshold = 0.8
-    loc = np.where( res >= threshold)
+#     # Sélection des meilleurs matched objects
+#     threshold = 0.8
+#     loc = np.where( res >= threshold)
 
-    # Affichage de la boite englobante de chaque objet détecté
-    for pt in zip(*loc[::-1]):
-        cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-
+#     # Affichage de la boite englobante de chaque objet détecté
+#     for pt in zip(*loc[::-1]):
+#         cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
 def souris(event, x, y, flags, param):
     global lo, hi, color, hsv_px
@@ -82,6 +97,11 @@ def souris(event, x, y, flags, param):
     lo[0]=color-10
     hi[0]=color+10
 
+def depth_of_selection(x,y,depth_frame):
+    if depth_frame:
+        depth = depth_frame.get_distance(x,y)
+        return([x,y,depth])
+
 color=100
 
 lo=np.array([color-5, 100, 50])
@@ -90,36 +110,15 @@ hi=np.array([color+5, 255,255])
 color_info=(0, 0, 255)
 
 
-# connect to a sensor (0: webcam)
-pipeline = rs.pipeline()
-config = rs.config()
 
-pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
-device_product_line = str(device.get_info(rs.camera_info.product_line))
-
-found_rgb = True
-for s in device.sensors:
-    if s.get_info(rs.camera_info.name) == 'RGB Camera':
-        found_rgb = True
-
-if not (found_rgb):
-    print("Depth camera required !!!")
-    exit(0)
-
-config.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 60)
-
-# Start streaming
-pipeline.start(config)
+align_to = rs.stream.depth
+align = rs.align(align_to)
 
 cap=cv2.VideoCapture(0)
 cv2.namedWindow('Camera')
 cv2.setMouseCallback('Camera', souris)
 hsv_px = [0,0,0]
 
-# Creating morphological kernel
-kernel = np.ones((3, 3), np.uint8)
 
 while True :
     # Wait for a coherent tuple of frames: depth, color and accel
@@ -127,14 +126,12 @@ while True :
 
     color_frame = frames.first(rs.stream.color)
 
-    color_image = np.asanyarray(color_frame.get_data())
+    aligned_frames =  align.process(frames)
+    depth_frame = aligned_frames.get_depth_frame()
+    aligned_color_frame = aligned_frames.get_color_frame()
 
-    image=cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
-    mask=cv2.inRange(image, lo, hi)
-    mask=cv2.erode(mask, kernel, iterations=1)
-    mask=cv2.dilate(mask, kernel, iterations=1)
-    image2=cv2.bitwise_and(color_image, color_image, mask= mask)
-    cv2.putText(color_image, "Couleur: {:d}".format(color), (10, 30), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
+    # color_image = np.asanyarray(aligned_color_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
 
     # Affichage des composantes HSV sous la souris sur l'image
     pixel_hsv = " ".join(str(values) for values in hsv_px)
@@ -144,17 +141,16 @@ while True :
     
     mask,vert=filtre_vert(color_image)
 
-    contours(color_image,mask,vert)
+    contours(color_image,mask,vert,depth_frame)
 
     # template_matching("./tuto_vision/img/template.png",color_image)
 
     # Display cropped image
     cv2.imshow("Camera", color_image)
     # cv2.imshow('image2', image2)
-    # cv2.imshow('Mask', mask)
+    cv2.imshow('Mask', mask)
     cv2.imshow("Vert",vert)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
-
